2f9a575bff681afd92c780f3fd101220001e6d60
Haroon Talib <haroontalib8029@gamil.com>
2025-02-11 15:37:34 +0500

    login controller co

--- src/controllers/authControllers/authControllers.ts
+++ src/controllers/authControllers/authControllers.ts
@@ -8,16 +8,23 @@ import bcrypt from "bcrypt";
 import { Send_Verify_Otp } from "../../services/emailVerify";
 import jwt from "jsonwebtoken";
 import { config } from "../../config/config";
+import _ from "lodash";
 
 // scheema to for the inputs validations from the user
-const Auth_Schema = Joi.object({
+const Auth_Schema_signup = Joi.object({
   user_name: Joi.string().min(5).max(50).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(6).required(),
   user_role: Joi.string().valid(UserRole.Admin, UserRole.User),
   otp: Joi.number(),
   otp_expiry: Joi.date(),
   active: Joi.boolean(),
+});
+
+// scheema for login user
+const Auth_Schema_login = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
 
 export const Register_User = async (
@@ -25,9 +32,9 @@ export const Register_User = async (
   res: Response
 ): Promise<any> => {
   try {
-    const { error, value } = Auth_Schema.validate(req.body as authTypes);
+    const { error, value } = Auth_Schema_signup.validate(req.body as authTypes);
     if (error) {
-      return res.status(400).json({
+      return res.status(401).json({
         success: false,
         message: error.details.map((details) => details.message),
       });
@@ -65,21 +72,108 @@ export const Register_User = async (
     // send email to verify the user using otp
     const sentMail = Send_Verify_Otp(user.email, user.otp);
 
-    const token = jwt.sign({ userid: user.id }, config.jwt_secret as string);
+    // const token = jwt.sign({ userid: user.id }, config.jwt_secret as string);
+    // res.cookie("authToken", token, {
+    //   httpOnly: true,
+    //   sameSite: "none",
+    //   secure: false,
+    //   maxAge: config.jwt_expiry as any,
+    // });
+    return res.status(201).json({
+      success: true,
+      // message: "User Successfuly Registerd ",
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: `Internal server error: ${error}`,
+    });
+  }
+};
+
+// login controller
+
+export const Signin = async (req: Request, res: Response): Promise<any> => {
+  try {
+    const { error, value } = Auth_Schema_login.validate(req.body);
+    if (error) {
+      return res.status(401).json({
+        success: false,
+        message: error.details.map((details) => details.message),
+      });
+    }
+    const { password, email } = value;
+    const user_Repo = AppDataSource.getRepository(User);
+    const Existing_User = await user_Repo.findOne({ where: { email: email } });
+    if (!Existing_User) {
+      return res.status(409).json({
+        success: false,
+        message:
+          "User with this email Not exist. Please Register your account !",
+      });
+    }
+    const Compare_Password = await bcrypt.compare(
+      password,
+      Existing_User.password
+    );
+    if (!Compare_Password) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized person. Password not matched !",
+      });
+    }
+    if (Existing_User.isVerified === false) {
+      Existing_User.otp = null as any;
+      Existing_User.otp_expiry as any;
+      await user_Repo.save(Existing_User);
+
+      // otp generation function
+      const otp = otpgenerator.generate(6, {
+        upperCaseAlphabets: false,
+        lowerCaseAlphabets: false,
+        specialChars: false,
+      }) as any;
+
+      const otp_expiry = new Date(Date.now() + 5 * 60 * 1000); // otp expiry 5 minutes from now
+
+      Existing_User.otp = otp;
+      Existing_User.otp_expiry = otp_expiry;
+      await user_Repo.save(Existing_User);
+
+      // send email to verify the user using otp
+      const sentMail = Send_Verify_Otp(Existing_User.email, Existing_User.otp);
+      return res.status(409).json({
+        success: false,
+        message:
+          "User not verified,OTP sent to your email. Please first Verify your account using OTP !",
+      });
+    }
+    const user = _.omit(Existing_User, [
+      "password",
+      "otp_expiry",
+      "otp",
+      "createdAt",
+      "updatedAt",
+    ]);
+    console.log("response at login controller", user);
+    const token = jwt.sign({ user }, config.jwt_secret as string);
     res.cookie("authToken", token, {
       httpOnly: true,
       sameSite: "none",
       secure: false,
-      maxAge: config.jwt_expiry as any,
+      maxAge: config.jwt_expiry as unknown as number,
     });
-
-    if (token) {
-      return res.status(201).json({
-        success: true,
-        message: "User Successfuly Registerd ",
-        user,
+    if (!token) {
+      return res.status(404).json({
+        success: false,
+        message: "somthing went worng while creating Jwt",
       });
     }
+    return res.status(200).json({
+      success: true,
+      message: "Login Successfuly",
+      user,
+    });
   } catch (error: any) {
     return res.status(500).json({
       success: false,
--- src/routes/authRoutes/authRoutes.ts
+++ src/routes/authRoutes/authRoutes.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import { Register_User } from "../../controllers/authControllers/authControllers";
+import {
+  Register_User,
+  Signin,
+} from "../../controllers/authControllers/authControllers";
 import { OTP_VERIFY } from "../../controllers/authVerifyOTP";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/register", Register_User);
 userRoutes.post("/verify_otp", OTP_VERIFY);
+userRoutes.post("/login", Signin);
 
 export default userRoutes;
